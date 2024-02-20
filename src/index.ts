85c24533fe12e8e565b6a92fef53459203b33101
Sobebar Ali <sobebar_ali17@hotmail.com>
2024-02-21 00:18:08 +0530

    Started

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
