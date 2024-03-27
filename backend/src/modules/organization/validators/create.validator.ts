f6836c9a611d0106e6ffd29d2887d8bd833c9073
Sobebar Ali <sobebar_ali17@hotmail.com>
2024-03-27 05:51:58 +0530

    Add prettier to backend dependencies

--- backend/src/modules/organization/validators/create.validator.ts
+++ backend/src/modules/organization/validators/create.validator.ts
@@ -1,6 +1,7 @@
 import Joi from "joi";
+import { typePayload } from "../types/create.types";
 
-const createSchema = Joi.object({
+const createSchema = Joi.object<typePayload>({
   name: Joi.string().min(2).max(100).required(),
   description: Joi.string().min(2).max(255).required(),
 });
